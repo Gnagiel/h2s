90a95b062a4ef5a668826da28e94b1f004b58e14
Guillaume Nagiel <guillaumenagiel@MacBook-Pro-de-Guillaume.local>
2018-03-02 13:41:42 +0100

    Ajout des commentaires doxygen

--- JS/add_perso.js
+++ JS/add_perso.js
@@ -1,15 +1,23 @@
+/**
+ * \file          add_perso.js
+ * \author    Guillaume Nagiel
+ * \version   1.0
+ * \date       26 Janvier 2018
+ * \brief       Test.
+ * \details    Ce fichier est la base du jeu.
+ */
  function auto_load(){
   $.ajax({
     url: 'index.php?action=cheat',
     cache: false,
     success: function(data){
        $("#main_page").html(data);
-    } 
+    }
   });
 }
 
 $(document).ready(function(){
-		
+
 	$(".add_perso").submit(function() {
 		s = $(this).serialize();
 		$.ajax({
