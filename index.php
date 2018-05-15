54de1b87178969f9f7454c8052cd24a879d1a49c
Guillaume Nagiel <guillaumenagiel@MacBook-Pro-de-Guillaume-2.local>
2018-05-15 16:36:52 +0200

    Modif doc doxygen

--- index.php
+++ index.php
@@ -1,57 +1,4 @@
 <?php
-/*! \mainpage Manuel
- - \subpage intro
- - \subpage doc_user
- */
-
-
-/*! \page intro Introduction
-  *
-  * \section desc Description
-  * Jeu en construction. Jeu de cartes à collectionner, constitution d'un deck, combats PVE et PVP automatiques.
-  *
-  * \section link Lien
-  * <a href="../../index.php">Lien vers le jeu</a>
-  *
-  * \section inclusion Arbre d'inclusion des fichiers php
-  * <img src="../../doc/monGraphe.svg" style="width:70%;border:1px solid grey;"/>
-  *
-  * \section BDD Schéma BDD
-  * <img src="../../sql/model.png" style="width:100%;border:1px solid grey;"/>
- */
-
- /*! \page doc_user Documentation utilisateur
-   *
-   * \section connexion Page de connexion
-   * Page de connexion et d'inscription au jeu.
-   * <img src="../../doc/img/connexion.png" style="width:100%;border:1px solid grey;"/>
-   *
-   * \section accueil Page d'accueil
-   * Page d'accueil du jeu. Nous trouverons içi le menu permettant d'acceder à toutes les fonctinnalitées du jeu.
-   * <img src="../../doc/img/accueil.png" style="width:100%;border:1px solid grey;"/>
-   *
-   * \section cheat Menu développeur
-   * Permet de tricher, afin de permettre au développeur de tester toutes les fonctionnalitées.
-   *
-   * \subsection cheat_team Page de modification de l'équipe
-   * Nous pouvons dans cet onglet modifier notre équipe.
-   * <img src="../../doc/img/cheat_team.png" style="width:100%;border:1px solid grey;"/>
-   *
-   * \subsection cheat_perso Page de modification des personnages
-   * En construction.
-   * <img src="../../doc/img/cheat_perso.png" style="width:100%;border:1px solid grey;"/>
-   *
-   * \subsection cheat_stuff Page de modification du stuff des personnages
-   * Cette section permet d'améliorer son équipement pour des tests.
-   * <img src="../../doc/img/cheat_stuff.png" style="width:100%;border:1px solid grey;"/>
-   *
-   * \section stuff Menu d'amélioration de l'équipement
-   * Cette section permet d'améliorer son équipement en dépensant de l'argent, on selectionne d'abord son personnage.
-   * <img src="../../doc/img/stuff.png" style="width:100%;border:1px solid grey;"/>
-   * Puis on sélectionne l'équipement à évoluer, une fenêtre s'ouvre. On pourra up niveau par niveau ou tous les niveaux disponibles d'un coup.
-   * <img src="../../doc/img/stuff_modal.png" style="width:40%;border:1px solid grey;"/>
-  */
-
 /**
  * \file          Index.php
  * \author    Guillaume Nagiel
@@ -62,15 +9,13 @@
  * \todo  Il reste à implémenter les scripts de combats PVE et PVP.
  */
 
- 	/**
- 	 * \fn chargerClass($class)
-	 * \brief       Charger les class à la demande
-	 * \details    Permet de charger les class uniquement si elles sont utilisées via la fonction spl_autoload_register('chargerClass'). (cf #$classe)
-	 * \param    $class         Class à instancier.
-	 * \return    Rien.
-	 */
-
-
+/**
+ * \fn chargerClass($class)
+ * \brief       Charger les class à la demande
+ * \details    Permet de charger les class uniquement si elles sont utilisées via la fonction spl_autoload_register('chargerClass'). (cf #$classe)
+ * \param    $class         Class à instancier.
+ * \return    Rien.
+ */
 
 function chargerClass($class)
 {
